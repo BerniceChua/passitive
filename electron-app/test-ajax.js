4476318b119fb01e06e12329103b6bbeea2284ca
BerniceChua <Bernice.Chua.415@gmail.com>
2016-08-28 05:59:16 -0700

    Copy form that accepts new passwords into overlay div of add/update.

--- electron-app/test-ajax.js
+++ electron-app/test-ajax.js
@@ -5,15 +5,18 @@ console.log(clipboard.readText('selection'))
 $('#submit-words').submit(function(event) {
   event.preventDefault();
   console.log("submitted")
+  console.log($('#submit-words').serialize())
   $.ajax({
     type: "POST",
     data: $('#submit-words').serialize()
   }).done(function(data) {
       console.log("Inside")
-      console.log( $('#answer').val() )
+      // console.log( $('#answer').val() )
       console.log( $('input').val() )
+      console.log('using $(body).find()')
+      console.log( $('body').find( $('#submit-words').serialize() ) )
 
-      clipboard.write({text: $('input').val() })
+      clipboard.write({text: $('#answer').val() })
   }).fail(function(responseError){
     console.error(responseError)
   })
