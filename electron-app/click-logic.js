4cc81f1a286a892ec4bbdcf657b0a64f3212ba01
BerniceChua <Bernice.Chua.415@gmail.com>
2016-08-28 06:00:11 -0700

    Add overlays for both 'Retrieve' & 'Add/Update'.

--- electron-app/click-logic.js
+++ electron-app/click-logic.js
@@ -27,11 +27,39 @@ $('#add-pw').click(function(event) {
   event.preventDefault();
   console.log("clicked 'add-pw")
 
+  addPWOverlay()
 
+  console.log( $('body').find() )
 }); 
 
 $('#retrieve-pw').click(function(event) {
   event.preventDefault();
   console.log("clicked 'add-pw")
+
+  retrievePWOverlay()
+
+  let $closePopup = $('body').find('.material-icons')
+  console.log( $closePopup )
+  $closePopup.click(function(event) {
+    event.preventDefault()
+
+    console.log('clicked close this popup')
+    
+    $('#retrieveDiv').remove()
+  })
 }); 
 
+
+function addPWOverlay() {
+  $('body').append( $addPWDiv )
+}
+
+let $addPWDiv = $( 
+  "<div id='object1' class='overlay'><p class='center-words'>Put Password Here</p><form class='center-words' id='submit-words' method='get'><label>login  <input id='login' type='text' name='login'/></label><br /><label>password  <input id='password' type='text' name='password'/></label><br /><input type='submit' value='Submit' /></form></div>" )
+
+function retrievePWOverlay() {
+  $('body').append( $retrievePWDiv )
+}
+
+let $retrievePWDiv = $( 
+  "<div id='retrieveDiv' class='overlay'><p class='center-words'>List Of Passwords Here</p><p><i class='material-icons w3-xxxlarge put-at-bottom-middle'>close</i></p></div>" )
