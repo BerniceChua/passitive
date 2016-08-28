48a30f897f7375c97e8cf5bb3c1e9e66f305b3be
BerniceChua <Bernice.Chua.415@gmail.com>
2016-08-28 01:21:55 -0700

    Add functionality to import file.

--- electron-app/click-logic.js
+++ electron-app/click-logic.js
@@ -1,21 +1,37 @@
-const {dialog} = require('electron')
+
+var fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
+
+const {dialog} = require('electron').remote
 
 $('#bulk-add-pw').click(function(event) {
   event.preventDefault();
   console.log("clicked 'bulk-add-pw")
 
-  console.log(dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']}))
-  // $.ajax({
-  //   type: "POST",
-  //   data: $('#submit-words').serialize()
-  // }).done(function(data) {
-  //     console.log("Inside")
-  //     console.log( $('#answer').val() )
-  //     console.log( $('input').val() )
-
-  //     clipboard.write({text: $('input').val() })
-  // }).fail(function(responseError){
-  //   console.error(responseError)
-  // })
-
+  dialog.showSaveDialog(function (fileName) {
+    if (fileName === undefined){
+      console.log("You didn't save the file");
+      return;
+    }
+    // fileName is a string that contains the path and filename created in the save file dialog.  
+    fs.writeFile(fileName, content, function (err) {
+      if(err){
+        alert("An error ocurred creating the file "+ err.message)
+      }
+                    
+      alert("The file has been succesfully saved");
+    });
+  });
 });
+
+$('#add-pw').click(function(event) {
+  event.preventDefault();
+  console.log("clicked 'add-pw")
+
+
+}); 
+
+$('#retrieve-pw').click(function(event) {
+  event.preventDefault();
+  console.log("clicked 'add-pw")
+}); 
+
